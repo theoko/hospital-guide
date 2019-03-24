232600fab7bdd397f7d8187defecbcccf16940bb
miandoli <miandoli8@gmail.com>
2019-03-24 12:32:44 -0400

    Added timer.

--- src/main/java/map/PathFinder.java
+++ src/main/java/map/PathFinder.java
@@ -3,7 +3,13 @@ package map;
 public class PathFinder {
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         // TODO: Fix path names for CSV files
         Map map = MapParser.parse("C:\\Users\\Miandoli8\\Documents\\College\\Soft Eng\\App\\out\\production\\resources\\csv\\nodes.csv", "C:\\Users\\Miandoli8\\Documents\\College\\Soft Eng\\App\\out\\production\\resources\\csv\\edges.csv");
+
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Time taken: " + totalTime);
     }
 }
