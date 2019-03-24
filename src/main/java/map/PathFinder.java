647ceccb4d456957476b8793b5a1248b51a54cba
miandoli <miandoli8@gmail.com>
2019-03-24 15:05:02 -0400

    New branch a-star

--- src/main/java/map/PathFinder.java
+++ src/main/java/map/PathFinder.java
@@ -4,8 +4,7 @@ public class PathFinder {
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-
-        // TODO: Fix path names for CSV files
+        
         Map map = MapParser.parse(PathFinder.class.getResource("/data/nodes.csv").getFile(), PathFinder.class.getResource("/data/edges.csv").getFile());
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
