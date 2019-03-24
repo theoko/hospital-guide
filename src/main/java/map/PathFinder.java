b440f9e3663697434777b251e5991c06e088440e
miandoli <miandoli8@gmail.com>
2019-03-24 14:50:51 -0400

    Fixed path for parser.

--- src/main/java/map/PathFinder.java
+++ src/main/java/map/PathFinder.java
@@ -6,8 +6,7 @@ public class PathFinder {
         long startTime = System.currentTimeMillis();
 
         // TODO: Fix path names for CSV files
-        Map map = MapParser.parse("C:\\Users\\Miandoli8\\Documents\\College\\Soft Eng\\App\\out\\production\\resources\\csv\\nodes.csv", "C:\\Users\\Miandoli8\\Documents\\College\\Soft Eng\\App\\out\\production\\resources\\csv\\edges.csv");
-
+        Map map = MapParser.parse(PathFinder.class.getResource("/data/nodes.csv").getFile(), PathFinder.class.getResource("/data/edges.csv").getFile());
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         System.out.println("Time taken: " + totalTime);
