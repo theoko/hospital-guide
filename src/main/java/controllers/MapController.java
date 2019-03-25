5b7d4abc01a0b77980254bb67f47f52924a2dd31
manasmehta18 <manasmehta.18@gmail.com>
2019-03-25 18:06:43 -0400

    mm drag for the entire pane fixed

--- src/main/java/controllers/MapController.java
+++ src/main/java/controllers/MapController.java
@@ -6,13 +6,23 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import sun.plugin.javascript.navig.Anchor;
 
 import java.awt.event.ActionEvent;
+import java.util.Stack;
 
 public class MapController {
 
     @FXML
-    ImageView floorOneMap;
+    private AnchorPane Map;
+
+    @FXML
+    JFXButton zoomIn;
+
+    @FXML
+    JFXButton zoomOut;
 
     double sceneX, sceneY;
     double translateX, translateY;
@@ -23,8 +33,8 @@ public class MapController {
         sceneX = event.getSceneX();
         sceneY = event.getSceneY();
 
-        translateX = ((ImageView) (event.getSource())).getTranslateX();
-        translateY = ((ImageView) (event.getSource())).getTranslateY();
+        translateX = ((AnchorPane) event.getSource()).getTranslateX();
+        translateY = ((AnchorPane) event.getSource()).getTranslateY();
     }
 
     public void floorOneMapOnMouseDragged(MouseEvent event) {
@@ -35,8 +45,8 @@ public class MapController {
         double newTranslateX = translateX + offsetX;
         double newTranslateY = translateY + offsetY;
 
-        ((ImageView) (event.getSource())).setTranslateX(newTranslateX);
-        ((ImageView) (event.getSource())).setTranslateY(newTranslateY);
+        ((AnchorPane) event.getSource()).setTranslateX(newTranslateX);
+        ((AnchorPane) event.getSource()).setTranslateY(newTranslateY);
     }
 
     public void floorOneMapScroll(ScrollEvent event) {
@@ -48,8 +58,12 @@ public class MapController {
 
     public void floorOneMapZoomIn(MouseEvent event) {
         System.out.println("scroll: " + event.toString());
-        
 
+
+   /*     zoomIn.addEventHandler(event.MOUSE_CLICKED, Event -> {
+                Map.fireEvent(Event.copyFor(Map, Map));
+            Event.consume();
+        });*/
     }
 
     public void floorOneMapZoomOut(MouseEvent event) {
