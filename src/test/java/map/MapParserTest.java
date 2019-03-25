29130667f4810d94b9927f74eef8e2eae4a0cb3e
miandoli <miandoli8@gmail.com>
2019-03-25 14:13:13 -0400

    Added test files.

---
+++ src/test/java/map/MapParserTest.java
@@ -0,0 +1,13 @@
+package map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapParserTest {
+
+    @Test
+    public void parse() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/map/PathFinderTest.java
@@ -0,0 +1,13 @@
+package map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PathFinderTest {
+
+    @Test
+    public void findPath() {
+        assertTrue(true);
+    }
+}
