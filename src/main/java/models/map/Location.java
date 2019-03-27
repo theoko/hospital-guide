96ea6b36d918b1ad2e7737b23f73cf5c6d10b4ee
miandoli <miandoli8@gmail.com>
2019-03-27 18:27:44 -0400

    Removed extraneous print line.

--- src/main/java/models/map/Location.java
+++ src/main/java/models/map/Location.java
@@ -69,7 +69,6 @@ public class Location {
     }
 
     public String[] getStrings() {
-        System.out.println(nodeID);
         return new String[]{nodeID, Integer.toString(xCord), Integer.toString(yCord), floor, building,
             nodeType.toString(), longName, shortName};
     }
