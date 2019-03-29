ada051c62a7b89e97a2f46cc4899de2d91daa56d
danyabaron <danyab552@gmail.com>
2019-03-29 16:12:59 -0400

    ui stuff

--- src/main/java/controllers/PathfinderController.java
+++ src/main/java/controllers/PathfinderController.java
@@ -1,4 +1,29 @@
 package controllers;
 
-public class PathfinderController {
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PathfinderController implements Initializable {
+
+
+    public Label lblNodeID;
+    public Label lblLocation;
+    public Label lblFloor;
+    public Label lblBuilding;
+    public Label lblNodeType;
+    public Label lblLongName;
+    public Label lblShortName;
+
+    public JFXButton btnDirections;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
