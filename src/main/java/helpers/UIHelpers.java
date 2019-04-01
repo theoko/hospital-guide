c3c166d2fb3faf28523942c48ad78b447cb7719e
KangarooMan64 <smithgar002@gmail.com>
2019-04-01 14:34:58 -0400

    Update UIHelpers.java

--- src/main/java/helpers/UIHelpers.java
+++ src/main/java/helpers/UIHelpers.java
@@ -1,6 +1,17 @@
 package helpers;
 
+import controllers.AdminMapController;
+import controllers.ScreenController;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Screen;
+import map.MapDisplay;
+import models.map.Edge;
+import models.map.Location;
+import models.map.Map;
+
+import java.util.HashMap;
 
 public class UIHelpers {
 
@@ -13,6 +24,38 @@ public class UIHelpers {
 
     public static double getScreenHeight() {
         return Screen.getPrimary().getBounds().getHeight();
+    }
+    public static void setUserNodeClickEvent(Circle c, Location loc, HashMap<String, Line> lstLines, Map map) {
+        c.setOnMouseClicked(event -> {
+            try {
+                event.consume();
+                ScreenController.popUp("info", loc, map, lstLines);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public static void setAdminNodeClickEvent(Circle c, Location loc) {
+
+        c.setOnMouseClicked(evt -> {
+            try {
+                evt.consume();
+//                AdminMapController.locationForEdgeSelected(loc);
+                ScreenController.popUp("edit", loc);
+            } catch (Exception e) {
+                throw new UnsupportedOperationException(e);
+            }
+        });
+    }
+    public static Line generateLineFromEdge(Edge e) {
+        Location start = e.getStart();
+        Location end = e.getEnd();
+        Line line = new Line(start.getxCord(), start.getyCord(), end.getxCord(), end.getyCord());
+        line.setStroke(Color.BLACK);
+        line.setStrokeWidth(MapDisplay.getEdgeWidth());
+//        lstLines.put(edge.getEdgeID(), line);
+        return line;
+//        pane.getChildren().add(line);
     }
 
 }
