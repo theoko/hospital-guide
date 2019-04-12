d7229f33546e8327e8c722d3848892ecebb46ada
Niko <nxgamarra@gmail.com>
2019-04-12 15:50:09 -0400

    UI progress so far, merge with dan

--- src/main/java/controllers/TransportationRequestController.java
+++ src/main/java/controllers/TransportationRequestController.java
@@ -2,7 +2,7 @@ package controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTabPane;
-import database.SanitationTable;
+import database.TransportationTable;
 import helpers.UserHelpers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,27 +12,28 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import map.MapDisplay;
 import map.PathFinder;
 import models.map.Location;
-import models.services.SanitationRequest;
+import models.services.TransportationRequest;
 import models.user.User;
 
-import java.util.List;
-import java.sql.Timestamp;
-import java.util.ResourceBundle;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class CustodianMapController extends MapController {
+public class TransportationRequestController  {
 
     public JFXButton btnSettings;
-    public TableView<SanitationRequest> tblData;
-    public TableColumn<SanitationRequest,String> tblRequestID;
-    public TableColumn<SanitationRequest,String> tblLocation;
-    public TableColumn<SanitationRequest,String> tblPriority;
-    public TableColumn<SanitationRequest,String> tblStatus;
-    public TableColumn<SanitationRequest,String> tblDescription;
-    public TableColumn<SanitationRequest,String> tblRequester;
-    public TableColumn<SanitationRequest,String> tblClaimTime;
-    public TableColumn<SanitationRequest,String> tblServicer;
-    public TableColumn<SanitationRequest,String> tblServiceTime;
+    public TableView<TransportationRequest> tblData;
+    //public TableColumn<SanitationRequest,String> tblRequestID;
+    public TableColumn<TransportationRequest,String> tblStartLoc;
+    public TableColumn<TransportationRequest,String> tblEndLoc;
+    public TableColumn<TransportationRequest,String> tblDetails;
+    public TableColumn<TransportationRequest,String> tblTime;
+    public TableColumn<TransportationRequest,String> tblDate;
+    public TableColumn<TransportationRequest,String> tblRequester;
+    public TableColumn<TransportationRequest,String> tblClaimTime;
+    public TableColumn<TransportationRequest,String> tblServicer;
+    public TableColumn<TransportationRequest,String> tblServiceTime;
 
 
     public JFXButton btnMarkDone;
@@ -41,21 +42,21 @@ public class CustodianMapController extends MapController {
     public JFXTabPane tabMenu;
     public JFXTabPane floorMenu;
 
-    ObservableList<SanitationRequest> spills = FXCollections.observableArrayList();
+    ObservableList<TransportationRequest> transports = FXCollections.observableArrayList();
 
-    @Override
+//    @Override
     public void initialize(URL location, ResourceBundle resources) {
-        super.initialize(location, resources);
-        MapDisplay.displayCust(this, panes, TextPane);
-        VisualRealtimeController.setPanMap(panFloor1);
-        initSanitation();
-        updateSanitation();
+//        super.initialize(location, resources);
+//        MapDisplay.displayCust(this, panes, TextPane);
+//        VisualRealtimeController.setPanMap(panFloor1);
+//        initSanitation();
+//        updateSanitation();
 
         tblData.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             btnNavigate.setDisable(false);
 
         });
-        SanitationRequest selected = tblData.getSelectionModel().getSelectedItem();
+        TransportationRequest selected = tblData.getSelectionModel().getSelectedItem();
         if(selected!=null)
         if(selected.getServicer()==null||selected.getServicer().equals(UserHelpers.getCurrentUser())) {//only enable claiming if unclaimed
             btnClaim.setDisable(false);
@@ -67,52 +68,53 @@ public class CustodianMapController extends MapController {
 
     private void initSanitation(){
 //        tblRequestID.setCellValueFactory(new PropertyValueFactory<>("RequestID"));
-        tblLocation.setCellValueFactory(new PropertyValueFactory<>("LocationShortName"));
-        tblPriority.setCellValueFactory(new PropertyValueFactory<>("Priority"));
-        tblStatus.setCellValueFactory(new PropertyValueFactory<>("Status"));
-        tblDescription.setCellValueFactory(new PropertyValueFactory<>("Description"));
+        tblStartLoc.setCellValueFactory(new PropertyValueFactory<>("StartLocation"));
+        tblEndLoc.setCellValueFactory(new PropertyValueFactory<>("EndLocation"));
+        //tblPriority.setCellValueFactory(new PropertyValueFactory<>("Priority"));
+        //tblStatus.setCellValueFactory(new PropertyValueFactory<>("Status"));
+        tblDetails.setCellValueFactory(new PropertyValueFactory<>("Description"));
         tblRequester.setCellValueFactory(new PropertyValueFactory<>("RequesterUserName"));
         tblClaimTime.setCellValueFactory(new PropertyValueFactory<>("ClaimedTime"));
         tblServiceTime.setCellValueFactory(new PropertyValueFactory<>("CompletedTime"));
         tblServicer.setCellValueFactory(new PropertyValueFactory<>("ServicerUserName"));
-        System.out.println(spills.toString());
-        tblData.setItems(spills);
+        System.out.println(transports.toString());
+        tblData.setItems(transports);
     }
 
     private void updateSanitation() {
-        List<SanitationRequest> lstReqs = SanitationTable.getSanitationRequests();
+        List<TransportationRequest> lstReqs = TransportationTable.getTransportationRequests();
         if(lstReqs!=null)
-         spills.addAll(lstReqs);
+         transports.addAll(lstReqs);
     }
 
     public void navigateTo(){
-        Location start = map.getLocation(MapController.getTempStart());
-        Location end = tblData.getSelectionModel().getSelectedItem().getLocation();
-
-        PathFinder.printPath(panes, TextPane, map, start, end);
-
-        String floor = start.getFloor();
-        int floorIndex;
-        switch (floor) {
-            case "1":
-                floorIndex = 3;
-                break;
-            case "2":
-                floorIndex = 1;
-                break;
-            case "3":
-                floorIndex = 0;
-                break;
-            case "L1":
-                floorIndex = 3;
-                break;
-            default:
-                floorIndex = 4;
-                break;
-        }
-
-        tabMenu.getSelectionModel().select(0);
-        floorMenu.getSelectionModel().select(floorIndex);
+//        Location start = map.getLocation(MapController.getTempStart());
+//        Location end = tblData.getSelectionModel().getSelectedItem().getLocation();
+//
+//        PathFinder.printPath(panes, TextPane, map, start, end);
+//
+//        String floor = start.getFloor();
+//        int floorIndex;
+//        switch (floor) {
+//            case "1":
+//                floorIndex = 3;
+//                break;
+//            case "2":
+//                floorIndex = 1;
+//                break;
+//            case "3":
+//                floorIndex = 0;
+//                break;
+//            case "L1":
+//                floorIndex = 3;
+//                break;
+//            default:
+//                floorIndex = 4;
+//                break;
+//        }
+//
+//        tabMenu.getSelectionModel().select(0);
+//        floorMenu.getSelectionModel().select(floorIndex);
     }
 
     public void tblClick(){
@@ -122,9 +124,9 @@ public class CustodianMapController extends MapController {
 
     public void updateAllBTNS(){
 
-        SanitationRequest selected = tblData.getSelectionModel().getSelectedItem();
+        TransportationRequest selected = tblData.getSelectionModel().getSelectedItem();
         User servicer = selected.getServicer();
-        boolean btnClaimEnabled = servicer == null || (servicer.equals(UserHelpers.getCurrentUser())&& selected.getStatus() == SanitationRequest.Status.INCOMPLETE);
+        boolean btnClaimEnabled = servicer == null || (servicer.equals(UserHelpers.getCurrentUser())&& selected.getStatus() == TransportationRequest.Status.INCOMPLETE);
         btnClaim.setDisable(!btnClaimEnabled);
         boolean btnMarkDoneEnabled = servicer != null && servicer.equals(UserHelpers.getCurrentUser());
         btnMarkDone.setDisable(!btnMarkDoneEnabled);
@@ -132,7 +134,7 @@ public class CustodianMapController extends MapController {
     }
     public void claimJob(){
 
-        SanitationRequest selected = tblData.getSelectionModel().getSelectedItem();
+        TransportationRequest selected = tblData.getSelectionModel().getSelectedItem();
 
 //        if (selected.getServicer() != null) {
 //        }else{
@@ -152,7 +154,7 @@ public class CustodianMapController extends MapController {
             selected.setClaimedTime(timestamp);
         }
 
-        SanitationTable.editSanitationRequest(selected);
+        TransportationTable.editTransportationRequest(selected);
 
         updateClaimBtn();
 
@@ -163,21 +165,21 @@ public class CustodianMapController extends MapController {
     }
 
     public void markDone(){
-        SanitationRequest selected = tblData.getSelectionModel().getSelectedItem();
+        TransportationRequest selected = tblData.getSelectionModel().getSelectedItem();
 //        if (selected.getUser().equals("")) {
 //            selected.setUser("user_temp");
 //        } else {
 //            selected.setUser("");
 //        }
-        if (selected.getStatus() == SanitationRequest.Status.COMPLETE) {
-            selected.setStatus(SanitationRequest.Status.INCOMPLETE);
+        if (selected.getStatus() == TransportationRequest.Status.COMPLETE) {
+            selected.setStatus(TransportationRequest.Status.INCOMPLETE);
             selected.setCompletedTime(null);
         } else {
-            selected.setStatus(SanitationRequest.Status.COMPLETE);
+            selected.setStatus(TransportationRequest.Status.COMPLETE);
             Timestamp timestamp = new Timestamp(System.currentTimeMillis());
             selected.setCompletedTime(timestamp);
         }
-        SanitationTable.editSanitationRequest(selected);
+        TransportationTable.editTransportationRequest(selected);
         tblData.refresh();
 
         updateAllBTNS();
@@ -186,7 +188,7 @@ public class CustodianMapController extends MapController {
     }
 
     private void updateMarkDoneBtn() {
-        if (tblData.getSelectionModel().getSelectedItem().getStatus() == SanitationRequest.Status.COMPLETE) {
+        if (tblData.getSelectionModel().getSelectedItem().getStatus() == TransportationRequest.Status.COMPLETE) {
             btnMarkDone.setText("Mark Incomplete");
         } else {
             btnMarkDone.setText("Mark Complete");
